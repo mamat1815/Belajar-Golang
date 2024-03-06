1e6398c75652d584be806dbb6cfd5a8909449c23
mamat1815 <mbahgendengngegame@gmail.com>
2024-03-06 21:39:37 +0700

    modifikasi tipe data

--- tipedata.go
+++ tipedata.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	
+	"strconv"// untuk casting ke string
+)
 
 func main () {
 	
@@ -14,5 +18,15 @@ func main () {
 	fmt.Println("Alamat Lama : " + alamat)
 	alamat = "Suropati 6" 
 	fmt.Println("Alamat Baru : " + alamat)
+	//Var Multiple
+	var (
+		firstName = "Muhammad"
+		lastName = "Afsar"
+		address = "Suropati"
+		addressNumber = 9
+
+	)
+	fmt.Println("Nama Saya " + firstName + " " + lastName)
+	fmt.Println("Alamat Tinggal Saya Jalan :" + address +"Nomor " + strconv.Itoa(addressNumber) )
 
 }
